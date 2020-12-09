c8d674a1349f880b08e3825c738f7a39cb55501e
Victoria <bravomacayov@gmail.com>
2020-12-09 12:38:40 +0100

    Cuarto commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/* // Get the modal
+var modal = document.getElementById('modalvideo');
+var video = document.getElementsByClassName ('iframe')
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+
+    
+  }  
+  }
+} */
